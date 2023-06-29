67599db9249c17434b3ffeb92583f5572e279b85
Maximiliano Gonzalez Pazo <silver.maxi7@gmail.com>
2023-06-28 23:26:19 -0300

    first commit

---
+++ Core.IO/Attributes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Omegacorp.Core.Model.Utilities
+{
+    public class OutsideModel : Attribute { }
+    public class IncrementalIdentity : Attribute { }
+    public class SharedIdentity : Attribute { }
+    public class NonIncrementalIdentity : Attribute { }
+    public class Descriptor : Attribute { }
+}
